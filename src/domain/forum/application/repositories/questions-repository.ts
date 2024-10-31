797d05903f1f60704f3d4f32b5145765a97f8026
Matheus Leal <matheus.mleall@gmail.com>
2024-10-31 20:12:46 -0300

    Created a new use case to fetch the questions by recent created at date, and implemented a paginate method to get 20 per page

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -1,7 +1,8 @@
-import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { Question } from "../../enterprise/entities/question";
+import { PaginationParams } from "@/core/repositories/pagination-params";
 
 export interface QuestionsRepository {
+  findManyRecent(params: PaginationParams): Promise<Question[]>;
   save(question: Question): Promise<void>;
   create(question: Question): Promise<void>;
   findBySlug(slug: string): Promise<Question | null>;
