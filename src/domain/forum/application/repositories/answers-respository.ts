fab485be7a47bd8644ee6657715366ee97bc3e35
Matheus Leal <matheus.mleall@gmail.com>
2024-11-28 20:29:05 -0300

    created a new answers repository

--- src/domain/forum/application/repositories/answers-respository.ts
+++ src/domain/forum/application/repositories/answers-respository.ts
@@ -1,8 +1,13 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { Answer } from "@/domain/forum/enterprise/entities/answer";
 
 export interface AnswersRepository {
   save(answer: Answer): Promise<void>;
   create(answer: Answer): Promise<void>;
   findById(id: string): Promise<Answer | null>;
+  findManyByQuestionId(
+    questionId: string,
+    params: PaginationParams
+  ): Promise<Answer[]>;
   delete(answer: Answer): Promise<void>;
 }
