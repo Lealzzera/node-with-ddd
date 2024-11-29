d6866f090f2b1c724b61082a811d8e8343973682
Matheus Leal <matheus.mleall@gmail.com>
2024-11-28 21:01:34 -0300

    removed useless imports

--- src/domain/forum/application/repositories/question-comments-repository.ts
+++ src/domain/forum/application/repositories/question-comments-repository.ts
@@ -1,5 +1,3 @@
-import { Question } from "../../enterprise/entities/question";
-import { PaginationParams } from "@/core/repositories/pagination-params";
 import { QuestionComment } from "../../enterprise/entities/question-comment";
 
 export interface QuestionCommentsRepository {
